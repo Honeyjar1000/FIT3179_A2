e19d7af8a8abbaaad22477ac7ea067a9c3afaec7
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2021-02-04 18:06:10 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ function changeAttribute(attributeName) {
   localStorage.setItem("attributeVar", attributeName);
   console.log("Update Attribute to: " + attributeName);
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
