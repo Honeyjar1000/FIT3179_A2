50e49cbbaadf703e1a7eaa4c3fb4938b54248145
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2021-02-04 18:25:58 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,15 +7,20 @@ function changeAttribute(attributeName) {
   console.log("Update Attribute to: " + attributeName);
   
   if (attributeName == 'Perceptions_of_corruption') {
-   var spec4 = "js/barChart2.vg.json";
+   var spec4 = "js/barChart.vg.json";
    vegaEmbed('#barChart', spec4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error)
-  } else if (attributeName == 'Generosity') {
-    var spec4 = "js/barChart3.vg.json";
+  } else if (attributeName == 'GDP_per_capita') {
+    var spec4 = "js/barChart1.vg.json";
     vegaEmbed('#barChart', spec4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error)
+  } else if (attributeName == 'Generosity') {
+    var spec4 = "js/barChart2.vg.json";
+    vegaEmbed('#barChart', spec4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error) 
   }
 }
 
