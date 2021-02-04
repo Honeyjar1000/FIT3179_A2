3818f2041d2eb28f6a9f5fc778651c38b7c1e0d0
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2021-02-04 17:26:01 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 // Controls which of the graphs is on display
 
 localStorage.setItem("attributeVar", "GDP_per_capita");
+
+function changeAttribute(attributeName) {
+  localStorage.setItem("attributeVar", attributeName);
+}
