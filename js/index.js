96b00764a5ac353813bb48281c08b2cc286547a4
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2021-02-04 18:20:25 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ localStorage.setItem("attributeVar", "GDP_per_capita");
 function changeAttribute(attributeName) {
   localStorage.setItem("attributeVar", attributeName);
   console.log("Update Attribute to: " + attributeName);
+  
+  if (attributeName == 'Perceptions_of_corruption') {
+   var spec4 = "js/barChart2.vg.json";
+   vegaEmbed('#barChart', spec4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error)
+  } else if (attributeName == 'Generosity') {
+    var spec4 = "js/barChart3.vg.json";
+    vegaEmbed('#barChart', spec4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error)
+  }
 }
 
 window.onclick = function(event) {
