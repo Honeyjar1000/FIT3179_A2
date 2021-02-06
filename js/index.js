91e21ff9081d4f25ff587016dd8c524c226f9506
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2021-02-06 12:54:28 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,20 @@ function changeAttribute(attributeName) {
   console.log("Update Attribute to: " + attributeName);
   
   if (attributeName == 'Perceptions_of_corruption') {
+   var spec3 = "js/pieChart.vg.json";
+  } else if (attributeName == 'GDP_per_capita') {
+    var spec3 = "js/pieChart2.vg.json";
+  } else if (attributeName == 'Generosity') {
+    var spec3 = "js/pieChart3.vg.json";
+  } else if (attributeName == 'Freedom_to_make_life_choices') {
+    var spec3 = "js/pieChart4.vg.json";
+  } else if (attributeName == 'Healthy_life_expectancy') {
+    var spec3 = "js/pieChart5.vg.json";
+  } else if (attributeName == 'Social_support') {
+    var spec3 = "js/pieChart6.vg.json";
+  }
+  
+  if (attributeName == 'Perceptions_of_corruption') {
    var spec4 = "js/barChart.vg.json";
   } else if (attributeName == 'GDP_per_capita') {
     var spec4 = "js/barChart2.vg.json";
